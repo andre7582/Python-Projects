ef3eaffacb8143b6d477d28365744c42e6825337
andre7582 <7582andre@gmail.com>
2022-02-08 11:46:13 -0300

    del files

--- cursoemvideo/desafio_03.py
+++
@@ -1,5 +0,0 @@
-print('====== Desafio 003 ======')
-# Crie um programa que leia dois números e mostre a soma entre eles.
-pri = int(input('Digite um número: '))
-seg = int(input('Digite outro número: '))
-print('A soma dos dois valores é:', pri + seg)
